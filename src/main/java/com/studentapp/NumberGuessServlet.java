f39a90688a0e676c229d0cd513613c2d5a380cc0
ideastarboy <63439535+ideastarboy@users.noreply.github.com>
2025-03-16 13:44:40 -0400

    Update NumberGuessServlet.java

--- src/main/java/com/studentapp/NumberGuessServlet.java
+++ src/main/java/com/studentapp/NumberGuessServlet.java
@@ -5,9 +5,10 @@ import java.io.PrintWriter;
 import java.util.Random;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 
 
 public class NumberGuessServlet extends HttpServlet {
