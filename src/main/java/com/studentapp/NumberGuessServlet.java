334c13727b239888d4cc1330db9237f282300121
ideastarboy <63439535+ideastarboy@users.noreply.github.com>
2025-03-16 11:59:27 -0400

    Update NumberGuessServlet.java

--- src/main/java/com/studentapp/NumberGuessServlet.java
+++ src/main/java/com/studentapp/NumberGuessServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
